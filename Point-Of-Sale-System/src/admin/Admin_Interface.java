fef1df1a320db201b9a610b498c68e90af201193
Nita <nitaantasuda@gmail.com>
2021-02-28 14:19:06 +0800

    Added pnlHistory,pnlInventory, pnlWelcome

--- Point-Of-Sale-System/src/admin/Admin_Interface.java
+++ Point-Of-Sale-System/src/admin/Admin_Interface.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package admin.Interface;
+package admin;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
@@ -11,11 +15,57 @@ package admin.Interface;
  */
 public class Admin_Interface extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Admin_Interface
-     */
+    
     public Admin_Interface() {
         initComponents();
+        
+        //Setting cardLayout
+        CardLayout objLayout = new CardLayout();
+        pnlContent.setLayout(objLayout);
+        
+        //calling the new panel welcome
+        pnlWelcome objWelcome = new pnlWelcome();
+        pnlContent.add(objWelcome, "1");
+        objLayout.show(pnlContent, "1");
+        
+        //calling new panel Inventory
+        pnlInventory objInventory = new pnlInventory();
+        pnlContent.add(objInventory, "2");
+        
+        //calling the new panel History
+        pnlHistory objHistory = new pnlHistory();
+        pnlContent.add(objHistory, "3");
+        
+         //showing the panel Welcome
+        btnHome.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				objLayout.show(pnlContent,"1");
+			}
+	});
+        
+        //showing the panel Inventory
+        btnInventory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				objLayout.show(pnlContent,"2");
+			}
+	});
+        
+         //showing the panel History
+        btnHistory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				objLayout.show(pnlContent,"3");
+			}
+	});
+        
+        this.setTitle("Admin Interface");
+        
+        
+        
+       
+        
+        
+        
+        
     }
 
     /**
@@ -28,6 +78,7 @@ public class Admin_Interface extends javax.swing.JFrame {
     private void initComponents() {
 
         pnlButtons = new javax.swing.JPanel();
+        btnHome = new javax.swing.JButton();
         btnCashier = new javax.swing.JButton();
         btnInventory = new javax.swing.JButton();
         btnHistory = new javax.swing.JButton();
@@ -35,18 +86,26 @@ public class Admin_Interface extends javax.swing.JFrame {
         btnUserLogbook = new javax.swing.JButton();
         btnAccounts = new javax.swing.JButton();
         btnFeedback = new javax.swing.JButton();
+        pnlContent = new javax.swing.JPanel();
         lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(950, 655));
+        setMinimumSize(new java.awt.Dimension(940, 660));
         setResizable(false);
         getContentPane().setLayout(null);
 
         pnlButtons.setOpaque(false);
-        pnlButtons.setLayout(new java.awt.GridLayout(7, 1, 15, 15));
+        pnlButtons.setLayout(new java.awt.GridLayout(8, 1, 15, 15));
 
-        btnCashier.setBackground(new java.awt.Color(0, 0, 0));
+        btnHome.setBackground(new java.awt.Color(1, 50, 67));
+        btnHome.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnHome.setForeground(new java.awt.Color(255, 255, 255));
+        btnHome.setText("Home");
+        pnlButtons.add(btnHome);
+
+        btnCashier.setBackground(new java.awt.Color(1, 50, 67));
         btnCashier.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnCashier.setForeground(new java.awt.Color(255, 255, 255));
         btnCashier.setText("Sales Input");
         btnCashier.setBorder(null);
         btnCashier.addActionListener(new java.awt.event.ActionListener() {
@@ -56,45 +115,66 @@ public class Admin_Interface extends javax.swing.JFrame {
         });
         pnlButtons.add(btnCashier);
 
-        btnInventory.setBackground(new java.awt.Color(0, 0, 0));
+        btnInventory.setBackground(new java.awt.Color(1, 50, 67));
         btnInventory.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnInventory.setForeground(new java.awt.Color(255, 255, 255));
         btnInventory.setText("Inventory");
+        btnInventory.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInventoryActionPerformed(evt);
+            }
+        });
         pnlButtons.add(btnInventory);
 
-        btnHistory.setBackground(new java.awt.Color(0, 0, 0));
+        btnHistory.setBackground(new java.awt.Color(1, 50, 67));
         btnHistory.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnHistory.setForeground(new java.awt.Color(255, 255, 255));
         btnHistory.setText("History");
         pnlButtons.add(btnHistory);
 
-        btnSalesMonitoring.setBackground(new java.awt.Color(0, 0, 0));
+        btnSalesMonitoring.setBackground(new java.awt.Color(1, 50, 67));
         btnSalesMonitoring.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnSalesMonitoring.setForeground(new java.awt.Color(255, 255, 255));
         btnSalesMonitoring.setText("Sales Monitoring");
         pnlButtons.add(btnSalesMonitoring);
 
-        btnUserLogbook.setBackground(new java.awt.Color(0, 0, 0));
+        btnUserLogbook.setBackground(new java.awt.Color(1, 50, 67));
         btnUserLogbook.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnUserLogbook.setForeground(new java.awt.Color(255, 255, 255));
         btnUserLogbook.setText("User Logbook");
         pnlButtons.add(btnUserLogbook);
 
-        btnAccounts.setBackground(new java.awt.Color(0, 0, 0));
+        btnAccounts.setBackground(new java.awt.Color(1, 50, 67));
         btnAccounts.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnAccounts.setForeground(new java.awt.Color(255, 255, 255));
         btnAccounts.setText("Accounts");
         pnlButtons.add(btnAccounts);
 
-        btnFeedback.setBackground(new java.awt.Color(0, 0, 0));
+        btnFeedback.setBackground(new java.awt.Color(1, 50, 67));
         btnFeedback.setFont(new java.awt.Font("Baskerville Old Face", 0, 18)); // NOI18N
+        btnFeedback.setForeground(new java.awt.Color(255, 255, 255));
         btnFeedback.setText("Feedback");
         pnlButtons.add(btnFeedback);
 
         getContentPane().add(pnlButtons);
-        pnlButtons.setBounds(10, 10, 195, 610);
+        pnlButtons.setBounds(10, 0, 200, 630);
 
-        lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/admin/Interface/Fast-Facts-about-PUP.jpg"))); // NOI18N
+        pnlContent.setBackground(new java.awt.Color(255, 153, 51));
+        pnlContent.setOpaque(false);
+        getContentPane().add(pnlContent);
+        pnlContent.setBounds(220, 10, 700, 610);
+
+        lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/admin/Fast-Facts-about-PUP.jpg"))); // NOI18N
         getContentPane().add(lblBackground);
-        lblBackground.setBounds(-40, -90, 1670, 900);
+        lblBackground.setBounds(-40, -80, 1670, 900);
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnInventoryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInventoryActionPerformed
+
+    }//GEN-LAST:event_btnInventoryActionPerformed
 
     private void btnCashierActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCashierActionPerformed
         // TODO add your handling code here:
@@ -140,10 +220,12 @@ public class Admin_Interface extends javax.swing.JFrame {
     private javax.swing.JButton btnCashier;
     private javax.swing.JButton btnFeedback;
     private javax.swing.JButton btnHistory;
+    private javax.swing.JButton btnHome;
     private javax.swing.JButton btnInventory;
     private javax.swing.JButton btnSalesMonitoring;
     private javax.swing.JButton btnUserLogbook;
     private javax.swing.JLabel lblBackground;
     private javax.swing.JPanel pnlButtons;
+    private javax.swing.JPanel pnlContent;
     // End of variables declaration//GEN-END:variables
 }
